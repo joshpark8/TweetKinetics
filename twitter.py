daa709097bd8108193940cbce7b77fe2152c0f7b
josh park <joshpark@joshs-MacBook-Pro.local>
2022-11-21 02:39:39 -0500

    fix typo in line 34

--- twitter.py
+++ twitter.py
@@ -9,7 +9,7 @@ search_url = "https://api.twitter.com/2/"
 
 # start_time,end_time,since_id,until_id,max_results,next_token,
 #   expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': 'from:joshparrk','tweet.fields': 'author_id'}
+query_params = {'query': 'from:chiw00k','tweet.fields': 'author_id'}
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
@@ -18,7 +18,7 @@ def bearer_oauth(r):
 
 def connect_to_endpoint(url, params):
     response = requests.get(url, auth=bearer_oauth, params=params)
-    print(response.status_code)
+    # print(response.status_code) # uncomment if 
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
