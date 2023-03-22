155baa9e1d0e0d351f728bd9e7f2ba843bef2227
Josh Park <84344359+joshpark8@users.noreply.github.com>
2023-03-22 10:36:30 -0500

    add requirements, consider changing project bc twitter api becoming paid

--- tweet.py
+++
@@ -1,8 +0,0 @@
-class Tweet:
-    def __init__(self, retweets, likes, replies, author):
-        self.retweets = retweets
-        self.likes = likes
-        self.replies = replies
-        self.author = author
-    
-    
